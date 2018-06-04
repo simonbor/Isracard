857c25732a4e640d1b1ce4f9d3a37ae46fd5e779
simonbor <simonbor@gmail.com>
2018-06-04 17:24:30 +0300

    init commit

---
+++ Controllers/GithubController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IsracardGithubSearch.Controllers
+{
+    public class GithubController : Controller
+    {
+        // GET: Github
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/core.js
@@ -0,0 +1,49 @@
+﻿var gitHubRepos = 'https://api.github.com/search/repositories?q=';
+
+(function () {
+
+    var app = angular.module("IsracardApp", ['ngSanitize']);
+
+    var searchController = function ($scope, $http) {
+
+        var onError = function (reason) {
+            $scope.error = reason.statusText + ' - ' + reason.status;
+            console.log($scope.new_body);
+        };
+
+        var fillResult = function (response) {
+            if (response && response.data && response.data.items) {
+                angular.forEach(response.data.items, function(rep) {
+                    this[rep.id] = rep;
+                }, $scope.repos);
+            }
+        };
+
+        $scope.getRepos = function () {
+            var keyword = $scope.keyword;
+            $scope.repos = {};
+
+            if (!keyword)
+                return;
+
+            $http({
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' },
+                    url: gitHubRepos + keyword
+                })
+                .then(fillResult, onError);
+        }
+
+        $scope.saveRepo = function(repoId) {
+            console.log('the ' + $scope.repos[repoId].full_name + ' saved');
+        }
+
+        $scope.caption = 'Github Search';
+        $scope.keyword = '';
+        $scope.repos = {};
+
+        $scope.getRepos();
+    };
+
+    app.controller("searchController", searchController);
+})();
